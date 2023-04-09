d203423b7d49425e05021f5b6a7c576e7cf5056d
rion96194 <rion_96194_y@yahoo.co.jp>
2023-04-09 13:43:20 +0900

    間違えている回数をカウント

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var questionCount = 0;
+var errorCount = 0;
+
 function generateQuestion() {
     // // ランダムに足し算または引き算を生成する
     // var operator = Math.random() < 0.5 ? '+' : '-';
@@ -35,6 +38,7 @@ function generateQuestion() {
 
     // 問題を表示する
 	document.getElementById('question').innerHTML = num1 + ' ' + operator + ' ' + num2 + ' = ';
+    questionCount += 1;
 }
 
 function checkAnswer() {
@@ -42,15 +46,16 @@ function checkAnswer() {
     var userAnswer = document.getElementById('answer').value;
     // 正解かどうかをチェックする
     if (userAnswer == answer) {
-        alert('正解！');
+        alert('せいかい！');
         // 次の問題を生成する
         generateQuestion();
         // 入力欄をクリアする
         document.getElementById('answer').value = '';
     } else {
-        alert('不正解。もう一度挑戦してみてください。');
+        alert('ちがいます。もういちどがんばろう！');
         // 入力欄クリア
         document.getElementById('answer').value = '';
+        errorCount += 1
     }
 }
 
@@ -60,4 +65,8 @@ function addToInput(value) {
 
 function clearInput() {
     document.getElementById('answer').value = '';
+}
+
+function answerCheck(){
+    alert('いま' + ' ' + questionCount + ' ' + 'もんめで、' + ' ' + errorCount + ' ' + 'かいまちがえています。');
 }
